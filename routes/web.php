cd64b331736602ce2cc96754fb534b1a3e5a157c
Michelangelo Ruisi <mruisi96@gmail.com>
2025-02-03 16:44:14 +0100

    aggiunta vista edit

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::post('/article/store', [ArticleController::class,'store'])->name('article
 Route::get('/article/index', [ArticleController::class,'index'])->name('article.index');
 Route::get('/article/show/{article}', [ArticleController::class,'show'])->name('article.show');
 
+Route::get('/article/{article}/edit', [ArticleController::class, 'edit'])->name('article.edit');
+Route::put('/article/{article}/update', [ArticleController::class, 'update'])->name('article.update');
+Route::delete('/article/{article}/destroy', [ArticleController::class, 'destroy'])->name('article.destroy');
+
+
